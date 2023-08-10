8c3ee55e8ff2976dfa2e3479c89aff0143821ff8
Janaina <jmtannus@gmail.com>
2023-08-10 17:21:42 -0300

    criando javascript, redimensionando imagens

--- script.js
+++ script.js
@@ -1,18 +1,59 @@
-const canvas = document.querySelector('#drawingCanvas');
-// ver informações no site: console.log(window); //
+let goldSunflower;
+let sunflower;
+let mouseIsDown = false;
+const imageWidth = 150;
+let paintbrush;
 
-canvas.width = window.innerWidth;
-canvas.Height = window.innerHeight;
-//innerHeight: 1146 / 929//
-//innerWidth: 1363 // 1120/
-const sunflower = document.createElement('img');
-const goldSunflower = document.createElement('img');
-
-sunflower.src = 'images/sunflower.png';
-goldSunflower.src = 'images/goldSunflower.png';
-
-const handleMouseMove = () => {
-    console.log('This is a function')
+const handleMouseDown = () => {
+   mouseIsDown = true;
+   let randomNumber = Math.random();
+   if (randomNumber < 0.5) {
+    paintbrush = sunflower;
+   } else {
+    paintbrush = goldSunflower;
+   }
 }
 
-handleMouseMove();
+const handleMouseUp = () => {
+    mouseIsDown = false;
+}
+  
+const handleMouseMove = (event) => {
+    const scrollAmount = document.scrollingElement.scrollTop;
+    const paintbrushHeight = paintbrush.naturalHeight;
+    const paintLimit = window.innerHeight - paintbrushHeight;
+    const left = event.clientX;
+    const top = Math.min(paintLimit, event.clientY + scrollAmount);
+    context.drawImage(paintbrush, left, top);
+}
+  
+const handleTouchMove = (event) => {
+   const scrollAmount = document.scrollingElement.scrollTop;
+   const paintbrushHeight = paintbrush.naturalHeight;
+   const paintLimit = window.innerHeight - paintbrushHeight;
+   const left = event.touches[0].clientX;
+   const top = Math.min(paintLimit, event.touches[0].clientY + scrollAmount);
+   context.drawImage(paintbrush, left, top);
+}
+
+const canvas = document.querySelector('#drawingCanvas');
+const context = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+goldSunflower = document.createElement('img');
+goldSunflower.src = 'images/goldSunflower.png';
+sunflower = document.createElement('img');
+sunflower.src = 'images/sunflower.png';
+
+canvas.addEventListener('mousemove', handleMouseMove);
+window.addEventListener('mousedown', handleMouseDown);
+window.addEventListener('mouseup', handleMouseUp);
+
+canvas.addEventListener('touchmove', handleTouchMove);
+window.addEventListener('touchstart', handleMouseDown);
+window.addEventListener('touchend', handleMouseUp);
+
+// dicas: ver informações no site: console.log(window); //
+//innerHeight: 1146 / 929//
+//innerWidth: 1363 // 1120/
